12c6bd07efbe89c1baf6c34ef0c8635890eed1c9
vaidehidahake <vaidehidahake0101@gmail.com>
2021-08-17 19:49:17 +0530

    addition

---
+++ C_Programs/as2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2,num3,addition;
+	printf("enter first number and second number and third number");
+	scanf("%d%d%d",&num1,&num2,&num3);
+	addition=num1+num2+num3;
+	printf("addition of numbers=%d",addition);
+	
+	
+	
+}
