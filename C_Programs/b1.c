8ba23e013d6d92cacf48691ddedd87b6dee76190
vaidehidahake <vaidehidahake0101@gmail.com>
2021-08-17 20:02:19 +0530

    binary operator

---
+++ C_Programs/b1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int x,y;
+	x=4;
+	y=8;
+	printf("x=%d,y=%d",x,y);
+	printf("\nx+y=%d",x+y);
+	printf("\nx-y=%d",x-y);
+	printf("\nx*y=%d",x*y);
+	printf("\nx/y=%d",x/y);
+	printf("\nx%y=%d",x%y);
+	
+}
