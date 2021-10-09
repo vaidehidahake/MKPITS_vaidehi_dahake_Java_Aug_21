d29112c415e6a63a7fc273f1b457edafdce17458
vaidehidahake <vaidehidahake0101@gmail.com>
2021-10-09 08:13:25 +0530

    filter writer and reader

---
+++ Java_Programs/FilterReaderExample.java
@@ -0,0 +1,37 @@
+import java.io.*;  
+    class CustomFilterReader extends FilterReader 
+    {  
+        CustomFilterReader(Reader in) 
+        {  
+            super(in);  
+        }  
+        public int read() throws IOException 
+        {  
+            int x = super.read();  
+            if ((char) x == ' ')  
+                return ((int) '?');  
+            else  
+                return x;  
+        }  
+    }  
+    public class FilterReaderExample 
+    {  
+        public static void main(String[] args) 
+        {  
+            try  {  
+                Reader reader = new FileReader("javaFile123.txt");  
+                CustomFilterReader fr = new CustomFilterReader(reader);  
+                int i;  
+                while ((i = fr.read()) != -1) 
+                {  
+                    System.out.print((char) i);  
+                }  
+                fr.close();  
+                reader.close();  
+            } 
+            catch (Exception e) 
+            {  
+                e.getMessage();  
+            }  
+        }  
+    }  
