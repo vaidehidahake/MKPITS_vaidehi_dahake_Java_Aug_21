5fc904f3fdc86aead81760c86ea81c8c2ff8db52
vaidehidahake <vaidehidahake0101@gmail.com>
2021-12-06 15:56:14 +0530

    practice

---
+++ Java_Programs/Number.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Number
+{
+  public static void main(String args[])
+  {
+	  Scanner s=new Scanner(System.in);
+      int num;
+      int Square;
+      Square=num*num;
+      System.out.println("Square of a Number:"+ Square);
+    }
+ }
